4b05e3de5973e092d407d6e2ba8c7a7180c92adb
Adam <aevans@agileand.me>
2017-10-10 21:24:53 +1100

    Fixed copy and paste issues in tmux

--- nix/.nixpkgs/config.nix
+++ nix/.nixpkgs/config.nix
@@ -1,3 +1,13 @@
 {
   allowUnfree = true;
+
+  packageOverrides = super: let self = super.pkgs; in with self; rec {
+    haskellPackages = super.haskellPackages.override {
+      overrides = self: super: with haskell.lib; {
+        # https://github.com/NixOS/nixpkgs/issues/29724
+        foundation = dontCheck super.foundation;
+      };
+    };
+  };
+
 }
