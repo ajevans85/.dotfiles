6de91552963c5917752a308c2a6d4e50b171f4db
Adam <aevans@agileand.me>
2017-10-16 22:16:12 +1100

    Cleaning up old files

--- ssh/.oh-my-zsh-custom/ssh.zsh
+++
@@ -1,4 +0,0 @@
-# Use ssh with merged config. Allows for combining work / personal configs
-alias ssh='cat ~/.ssh/conf.d/* > ~/.ssh/config; ssh'
-
-alias sshconfig='cat ~/.ssh/conf.d/* > ~/.ssh/config'
